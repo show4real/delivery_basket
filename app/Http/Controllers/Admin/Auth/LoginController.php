1dc5426a6ed2e2cb782d0766c8856cdca2069ca8
Showole Oladayo <show4ril@gmail.com>
2023-05-21 04:10:33 +0100

    remove recaptcha

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -35,27 +35,27 @@ class LoginController extends Controller
             'password' => 'required|min:6'
         ]);
 
-        $recaptcha = Helpers::get_business_settings('recaptcha');
-        if (isset($recaptcha) && $recaptcha['status'] == 1) {
-            $request->validate([
-                'g-recaptcha-response' => [
-                    function ($attribute, $value, $fail) {
-                        $secret_key = Helpers::get_business_settings('recaptcha')['secret_key'];
-                        $response = $value;
-                        $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . $secret_key . '&response=' . $response;
-                        $response = Http::get($url);
-                        $response = $response->json();
-                        if (!isset($response['success']) || !$response['success']) {
-                            $fail(translate('messages.ReCAPTCHA Failed'));
-                        }
-                    },
-                ],
-            ]);
-        } else if(strtolower(session('six_captcha')) != strtolower($request->custome_recaptcha))
-        {
-            Toastr::error(translate('messages.ReCAPTCHA Failed'));
-            return back();
-        }
+        // $recaptcha = Helpers::get_business_settings('recaptcha');
+        // if (isset($recaptcha) && $recaptcha['status'] == 1) {
+        //     $request->validate([
+        //         'g-recaptcha-response' => [
+        //             function ($attribute, $value, $fail) {
+        //                 $secret_key = Helpers::get_business_settings('recaptcha')['secret_key'];
+        //                 $response = $value;
+        //                 $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . $secret_key . '&response=' . $response;
+        //                 $response = Http::get($url);
+        //                 $response = $response->json();
+        //                 if (!isset($response['success']) || !$response['success']) {
+        //                     $fail(translate('messages.ReCAPTCHA Failed'));
+        //                 }
+        //             },
+        //         ],
+        //     ]);
+        // } else if(strtolower(session('six_captcha')) != strtolower($request->custome_recaptcha))
+        // {
+        //     Toastr::error(translate('messages.ReCAPTCHA Failed'));
+        //     return back();
+        // }
 
         if (auth('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
             $admin = Admin::find(auth('admin')->id());
